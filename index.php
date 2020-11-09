6e139b6850a60723e46be3086908e7a185e38261
Miguel Prates <20394@csmrouen.net>
2020-11-09 10:22:51 +0100

    init git

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// GIT est un outil de Versionning,
+
+// GITHUB est ce qu'on appel un hebergeur de repositoires GIT
+
+//git init, à faire une fois
+
+//git status, permettre de savoir où on en est
